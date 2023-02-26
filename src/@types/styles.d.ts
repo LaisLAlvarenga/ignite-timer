3657aeda6f458f055d54d48925920c8d5e7ba146
Lais Luderer Alvarenga <lais.alvarenga.global@gmail.com>
2023-02-26 11:24:38 -0300

    Tipagem dos temas
    Criando um arquivo de tipagem especifica, pois o TS não está sugerindo as propriedades disponíveis no Theme - defaultTheme

---
+++ src/@types/styles.d.ts
@@ -0,0 +1,14 @@
+/* Criando uma tipagem especifica para a aplicação, pois o TS não está exibindo as props do Theme */
+
+import "styled-components";
+import {defaultTheme} from '../styles/theme/default'
+
+//Armazenando dentro da var ThemeType as propriedades que o defaultTheme possui.
+type ThemeType = typeof defaultTheme;
+
+//Criando uma tipagem para o modulo "styled-components"
+declare module 'styled-components' {
+    export interface DefaultTheme extends ThemeType {}
+}
+
+//OBS - Arquivos .d, significa que é um arquivo de definição de tipos do JS. Não dev ter código js, apenas tipagens (interface)

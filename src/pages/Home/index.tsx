88de2e2c8fc3f5d27f58b13a05d003f35c487f35
Lais Luderer Alvarenga <lais.alvarenga.global@gmail.com>
2023-02-27 20:00:15 -0300

    Modelo UnControlled-Busca a informação somente quando precisamos dela.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { Play } from 'phosphor-react'
 import {
   ButtonContainer,
@@ -12,18 +11,19 @@ import {
 
 export function Home() {
   /*
-    Modelo de formulário Controlled:
-    Monitora em tempo real as mudanças no input e armazena todas as modificações no estado.
-    Vantagem: Por ser em tempo real existe uma facilidade para mostrar/desabilitar algo em tela. 
-    (Ex: Habilitar botão quando preencher input e desabilitar quando estiver vazio. )
-    Desvantagem: A cada vez que alteramos o estado, o React renderiza novamente todo o código. Dependendo do tamanho da aplicação isso pode ser um gargalo.
+    Modelo Uncontrolled:
+    Busca a informação no input somente quando precisamos dela.
+    Desvantagem: Perda da fluidez, como não temos o tempo real não conseguimos habilitar ou desabilitar algo de forma rápida. 
+    Vantagem: Melhora na performance.
   */
-  const [task, setTask] = useState('')
+  function handleSubmit(event) {
+    console.log(event.target.task.value)
+  }
 
   return (
     <>
       <HomeContainer>
-        <form>
+        <form onSubmit={handleSubmit}>
           <FormContainer>
             <label htmlFor="task">Vou trabalhar em</label>
             <TaskInput
@@ -32,8 +32,6 @@ export function Home() {
               id="task"
               placeholder="Dê um nome para o seu projeto"
               list="task-suggestions"
-              onChange={(e) => setTask(e.target.value)}
-              value={task}
             />
 
             {/* O <datalist> serve para ter dentro do input sugestões já fixas para o usuário. Futuramente, o que ele informar fica salvo nesse data list de sugestões */}
@@ -64,7 +62,7 @@ export function Home() {
             <span>0</span>
           </CountdownContainer>
 
-          <ButtonContainer disabled={task === ''}>
+          <ButtonContainer>
             <Play size={24} />
             Começar
           </ButtonContainer>
